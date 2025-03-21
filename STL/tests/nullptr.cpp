a016c8bd6cea10fc8d425e335aaee3eb24330a6e
root <root@YELSONCAI-PC0>
2025-03-21 17:47:49 +0800

    - Add STL/tests/nullptr.cpp.

---
+++ STL/tests/nullptr.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+class A {
+public:
+    A() {
+        std::cout << "A()" << std::endl;
+    }
+    ~A() {
+        std::cout << "~A()" << std::endl;
+    }
+
+    void func() {
+        std::cout << "func() - Success" << std::endl;
+    }
+
+    virtual void virtual_func() {
+        std::cout << "virtual_func() - Success" << std::endl;
+    }
+
+    static void static_func() {
+        std::cout << "static_func() - Success" << std::endl;
+    }
+
+    int val = 10;
+};
+
+
+int main() {
+    A* a = nullptr;
+    a->func(); // 可以执行
+    a->static_func(); // 可以执行
+    a->virtual_func(); // segmentation fault
+    std::cout << a->val << std::endl; // segmentation fault
+    delete a;
+    return 0;
+}
